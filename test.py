2c26ce22c847c223fcf7cbf5cc49fa4ee4a91961
sasankloki <sasank1130@gmail.com>
2020-08-09 18:59:27 +0200

    tests executed

--- test.py
+++ test.py
@@ -13,8 +13,12 @@ class AdvanceCalc(unittest.TestCase):
     def test_max(self):
 
         #Test script for taking  maximum value in a given list
-        reply=self.app.get("/max?X=15,35,200,500,459")
-        self.assertEquals(b'500\n',reply.data)
+        reply=self.app.get("/max?X=15,35,200,50,45")
+        self.assertEqual(b'200 \n',reply.data)
 
         reply=self.app.get("/max?X=15,35,20.7,50.78,45.9")
-        self.assertEquals(b'500\n',reply.data)
+        self.assertEqual(b'50.78 \n',reply.data)
+
+
+if __name__ == "__main__":
+    unittest.main()

8ccf24545db1f811477fc8cb1546f23a00236cb0
sasankloki <sasank1130@gmail.com>
2020-08-09 19:02:31 +0200

    tests executed

--- test.py
+++ test.py
@@ -14,7 +14,11 @@ class AdvanceCalc(unittest.TestCase):
 
         #Test script for taking  maniumum value in a given input
         reply=self.app.get("/min?X=15,1,57,37,8,34,5")
-        self.assertEquals(b'1\n',reply.data)
+        self.assertEqual(b'1\n',reply.data)
 
         reply=self.app.get("/min?X=1,2,45,7,0,7.5,3")
-        self.assertEquals(b'0\n',reply.data)
+        self.assertEqual(b'0\n',reply.data)
+
+
+if __name__ == "__main__":
+    unittest.main()
